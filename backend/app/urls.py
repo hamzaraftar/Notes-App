42199426074754bdfd8c0e1d70997391ae7b2bac
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-12 18:30:26 +0500

    makign update route

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,9 @@
 from app import views
 from django.urls import path
 urlpatterns = [
-    path('blogs/', views.blog_list, name='blog_list'),
+    path('blog/', views.blog_list, name='blog_list'),
+    path('blog/<str:pk>/', views.blog_detail, name='blog_detail'),
     path('blog_create/', views.blog_create, name='blog_create'),
+    path('blog_update/<str:pk>/', views.blog_update, name='blog_update'),
     
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -12,6 +12,16 @@ def blog_list(request):
     serializer = BlogSerializers(blogs, many=True)
     return Response(serializer.data)
 
+# Get a single blog
+@api_view(['GET'])
+def blog_detail(request, pk):
+    try:
+        blog = Blogs.objects.get(id=pk)
+    except Blogs.DoesNotExist:
+        return Response({"error": "Blog not found"})    
+    serializer = BlogSerializers(blog, many=False)
+    return Response(serializer.data)
+
 # Creaate a new blog
 @api_view(['POST'])
 def blog_create(request):
@@ -20,3 +30,17 @@ def blog_create(request):
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
+    return Response(serializer.errors)
+
+# Update a blog
+@api_view(['PUT'])
+def blog_update(request, pk):
+    if request.method == 'PUT':
+        try:
+            blog = Blogs.objects.get(id=pk)    
+            serializer = BlogSerializers(instance=blog, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({"error": str(e)})   
